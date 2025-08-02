60876dcef6fa94c3633c8e8fc7cbf379cfc916d7
makxxic <legendarygalaxyic@gmail.com>
2025-08-02 12:28:28 +0300

    anwers to week1 assignment

--- week1_assignment.py
+++ week1_assignment.py
@@ -1,1 +1,23 @@
+# Get user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose an operation (+, -, *, /): ")
 
+def calculator(num1, num2, operation):
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Error: Invalid operation"
+
+#call the calculator function    
+result = calculator(num1, num2, operation)
+print(f"The result is: {result}")
